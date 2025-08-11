e2a922bcb79e2702465f30dea420da6e6d8c2921
raseward14 <raseward14@gmail.com>
2025-08-11 17:57:29 -0600

    form styles applied and footer created in app component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
