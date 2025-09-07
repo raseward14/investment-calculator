0393be23aca10835b0caac1268833f3582e36e3d
raseward14 <raseward14@gmail.com>
2025-09-06 22:44:19 -0600

    migrated to using modules for practice - standalone components are common practice

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,13 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-import { InvestmentResultsComponent } from '../investment-results/investment-results.component';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule, InvestmentResultsComponent],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
